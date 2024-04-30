09cf0535eddd03ad72ce87d234bd54d65ea321bc
Martin Klemsa <m.klemsa@gmail.com>
2024-04-30 10:45:49 +0200

    Added material-icon-button

---
+++ material-icon-button/material-icon-button-toggle.js
@@ -0,0 +1,65 @@
+﻿(function(root, factory) {
+	const toolsPackageName = '@skolaris/knockout-tools',
+		mdcIconButtonPackageName = '@material/icon-button';
+
+	if (typeof define === 'function' && define.amd) {
+		//AMD. Register as an anonymous module.
+		define([toolsPackageName, mdcIconButtonPackageName], factory);
+	}
+	else if (typeof module === 'object' && module.exports) {
+		//Node. Does not work with strict CommonJS, but only CommonJS-like environments that support module.exports like Node.
+		module.exports = factory(require(toolsPackageName), require(mdcIconButtonPackageName));
+	}
+	else {
+		//Browser globals (root is window)
+		root.knockoutMdc = root.knockoutMdc || {};
+		root.knockoutMdc['material-icon-button-toggle'] = factory(root.knockoutTools, root.mdc['icon-button']);
+	}
+}(typeof self !== 'undefined' ? self : this, function(tools, materialIconButton) {
+
+	const MaterialIconButtonToggle = function(params) {
+		this.value = params.value;
+		this.icons = params.icons;
+		this.enable = tools.isComponentEnabled(params);
+
+		//component lifetime
+		this.mdcIconButtonToggle = null;
+		this._valueSubscription = null;
+	};
+	MaterialIconButtonToggle.prototype = {
+		'koDescendantsComplete': function(node) {
+			if (!node.isConnected)
+				return;
+
+			this.mdcIconButtonToggle = new materialIconButton.MDCIconButtonToggle(node.querySelector('.mdc-icon-button'));
+			this.mdcIconButtonToggle.on = this.value();
+			this._valueSubscription = this.value.subscribe(newVal => {
+				this.mdcIconButtonToggle.on = newVal;
+			});
+		},
+		'dispose': function() {
+			this._valueSubscription?.dispose();
+			this.mdcIconButtonToggle?.destroy();
+		},
+
+		'onChange': function(vm, event) {
+			this.value(event.detail.isOn);
+		}
+	};
+
+	return {
+		'viewModel': MaterialIconButtonToggle,
+		'template':
+`<button class="mdc-icon-button" data-bind="enable: enable, event: {'MDCIconButtonToggle:change': onChange}">
+	<div class="mdc-icon-button__ripple"></div>
+	<span class="mdc-icon-button__focus-ring"></span>
+	<!-- ko if: icons -->
+	<i class="material-icons mdc-icon-button__icon mdc-icon-button__icon--on">{{icons[0]}}</i>
+	<i class="material-icons mdc-icon-button__icon">{{icons[1]}}</i>
+	<!-- /ko -->
+	<!-- ko ifnot: icons -->
+	<!-- ko template: {'nodes': $componentTemplateNodes} --><!-- /ko -->
+	<!-- /ko -->
+</button>`
+	};
+}));
---
+++ material-icon-button/material-icon-button.js
@@ -0,0 +1,53 @@
+﻿(function(root, factory) {
+	const toolsPackageName = '@skolaris/knockout-tools',
+		mdcRipplePackageName = '@material/ripple';
+
+	if (typeof define === 'function' && define.amd) {
+		//AMD. Register as an anonymous module.
+		define([toolsPackageName, mdcRipplePackageName], factory);
+	}
+	else if (typeof module === 'object' && module.exports) {
+		//Node. Does not work with strict CommonJS, but only CommonJS-like environments that support module.exports like Node.
+		module.exports = factory(require(toolsPackageName), require(mdcRipplePackageName));
+	}
+	else {
+		//Browser globals (root is window)
+		root.knockoutMdc = root.knockoutMdc || {};
+		root.knockoutMdc['material-icon-button'] = factory(root.knockoutTools, root.mdc.ripple);
+	}
+}(typeof self !== 'undefined' ? self : this, function(tools, materialRipple) {
+
+	const MaterialIconButton = function(params) {
+		this.click = params.click;
+		this.icon = params.icon;
+		this.enable = tools.isComponentEnabled(params);
+
+		//component lifetime
+		this.mdcRipple = null;
+	};
+	MaterialIconButton.prototype = {
+		'koDescendantsComplete': function(node) {
+			if (!node.isConnected)
+				return;
+
+			this.mdcRipple = new materialRipple.MDCRipple(node.querySelector('.mdc-icon-button'));
+			this.mdcRipple.unbounded = true;
+		},
+		'dispose': function() {
+			this.mdcRipple?.destroy();
+		}
+	};
+
+	return {
+		'viewModel': MaterialIconButton,
+		'template':
+`<button class="mdc-icon-button material-icons" data-bind="enable: enable, click: click">
+	<div class="mdc-icon-button__ripple"></div>
+	<span class="mdc-icon-button__focus-ring"></span>
+	<!-- ko if: icon -->{{icon}}<!-- /ko -->
+	<!-- ko ifnot: icon -->
+	<!-- ko template: {'nodes': $componentTemplateNodes, 'data': $parent} --><!-- /ko -->
+	<!-- /ko -->
+</button>`
+	};
+}));
