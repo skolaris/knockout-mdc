c53d4a6f2e4e56a7ca832524504d7472380fc889
Martin Klemsa <m.klemsa@gmail.com>
2024-05-23 14:10:38 +0200

    Added material-banner and material-collapse

---
+++ material-banner/material-banner.js
@@ -0,0 +1,83 @@
+﻿(function(root, factory) {
+	const toolsPackageName = '@skolaris/knockout-tools';
+
+	if (typeof define === 'function' && define.amd) {
+		//AMD. Register as an anonymous module.
+		define([toolsPackageName], factory);
+	}
+	else if (typeof module === 'object' && module.exports) {
+		//Node. Does not work with strict CommonJS, but only CommonJS-like environments that support module.exports like Node.
+		module.exports = factory(require(toolsPackageName));
+	}
+	else {
+		//Browser globals (root is window)
+		root.knockoutMdc = root.knockoutMdc || {};
+		root.knockoutMdc['material-banner'] = factory(root.knockoutTools);
+	}
+}(typeof self !== 'undefined' ? self : this, function(tools) {
+
+	const MaterialBanner = function(params) {
+		this.icon = params.icon;
+		this.text = params.text;
+		this.buttons = params.buttons;
+		this.show = params.show;
+		this.dismissed = params.dismissed;
+		this.id = tools.getGuid();
+
+		const onButtonClick = index => {
+			if (this.buttons[index].dismiss)
+				this._hide();
+
+			if (typeof this.buttons[index].click === 'function')
+				this.buttons[index].click();
+		};
+		this.onButton0 = () => onButtonClick(0);
+		this.onButton1 = () => onButtonClick(1);
+	};
+	MaterialBanner.prototype = {
+		'_hide': function() {
+			this.dismissed(true);
+		}
+	};
+
+	return {
+		'viewModel': MaterialBanner,
+		'template':
+`<material-collapse params="opened: show">
+	<div class="material-banner__wrapper">
+		<div class="material-banner__message">
+			<!-- ko if: icon -->
+			<div class="material-banner__illustration">
+				<i class="material-icons">{{icon}}</i>
+			</div>
+			<!-- /ko -->
+			<div class="material-banner__text">
+				<span class="mdc-typography--body2" data-bind="html: text"></span>
+			</div>
+		</div>
+		<div class="material-banner__buttons">
+			<!-- ko using: buttons[0] -->
+			<!-- ko if: $data.href -->
+			<a data-bind="href: href" target="_blank" rel="noopener" tabindex="-1">
+				<material-button>{{text}}</material-button>
+			</a>
+			<!-- /ko -->
+			<!-- ko ifnot: $data.href -->
+			<material-button data-bind="click: $parent.onButton0">{{text}}</material-button>
+			<!-- /ko -->
+			<!-- /ko -->
+			<!-- ko with: buttons[1] -->
+			<!-- ko if: $data.href -->
+			<a data-bind="href: href" target="_blank" rel="noopener" tabindex="-1">
+				<material-button>{{text}}</material-button>
+			</a>
+			<!-- /ko -->
+			<!-- ko ifnot: $data.href -->
+			<material-button data-bind="click: $parent.onButton1">{{text}}</material-button>
+			<!-- /ko -->
+			<!-- /ko -->
+		</div>
+	</div>
+</material-collapse>`
+	};
+}));
---
+++ material-collapse/material-collapse.js
@@ -0,0 +1,72 @@
+﻿(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		//AMD. Register as an anonymous module.
+		define(factory);
+	}
+	else if (typeof module === 'object' && module.exports) {
+		//Node. Does not work with strict CommonJS, but only CommonJS-like environments that support module.exports like Node.
+		module.exports = factory();
+	}
+	else {
+		//Browser globals (root is window)
+		root.knockoutMdc = root.knockoutMdc || {};
+		root.knockoutMdc['material-collapse'] = factory();
+	}
+}(typeof self !== 'undefined' ? self : this, function() {
+
+	const transitionDuration = 175; //ms
+
+	var MaterialCollapse = function(params) {
+		this.opened = params.opened;
+		this.node = null;
+		this.content = null;
+		this._openedSubscription = null;
+		this._resizeObserver = null;
+	};
+	MaterialCollapse.prototype = {
+		'koDescendantsComplete': function(node) {
+			if (!node.isConnected)
+				return;
+
+			this.node = node;
+			this.content = node.querySelector('.material-collapse__content');
+			this._resizeObserver = new window.ResizeObserver(entries => {
+				for (const entry of entries) {
+					const height = entry.borderBoxSize[0].blockSize;
+					this._setMaxHeight(height);
+				}
+			});
+
+			this._layout();
+			this._openedSubscription = this.opened.subscribe(() => this._layout());
+		},
+		'dispose': function() {
+			this._openedSubscription?.dispose();
+			this._resizeObserver?.disconnect();
+		},
+
+		'_layout': function() {
+			if (this.opened()) {
+				this._resizeObserver.observe(this.content);
+				setTimeout(() => this.node.classList.add('material-collapse--expanded'), transitionDuration);
+			}
+			else {
+				this.node.classList.remove('material-collapse--expanded');
+				this._resizeObserver.unobserve(this.content);
+				this._setMaxHeight(null);
+			}
+		},
+		'_setMaxHeight': function(height) {
+			this.node.style.maxHeight = height === null ? height : `${height}px`;
+		}
+	};
+
+	const nodes = '$componentTemplateNodes';
+
+	return {
+		'viewModel': MaterialCollapse,
+		'template':
+`<div class="material-collapse__content"
+data-bind="template: {'nodes': ${nodes}.length > 0 ? ${nodes} : $parentContext.${nodes}, data: ${nodes}.length > 0 ? $parent : $parents[1]}"></div>`
+	};
+}));
