c4f2f3cef068c6a9eb295273ffc7aff37c4e3772
Martin Klemsa <m.klemsa@gmail.com>
2024-11-13 11:43:56 +0100

    Added material-list-item and material-tooltip

---
+++ material-list-item/material-list-item.js
@@ -0,0 +1,59 @@
+﻿(function(root, factory) {
+	const toolsPackageName = '@skolaris/knockout-tools';
+	if (typeof define === 'function' && define.amd) {
+		//AMD. Register as an anonymous module.
+		define([toolsPackageName], factory);
+	}
+	else if (typeof module === 'object' && module.exports) {
+		//Node. Does not work with strict CommonJS, but only CommonJS-like environments that support module.exports like Node.
+		module.exports = factory(require(toolsPackageName));
+	}
+	else {
+		//Browser globals (root is window)
+		root.knockoutMdc = root.knockoutMdc || {};
+		root.knockoutMdc['material-list-item'] = factory(root.knockoutTools);
+	}
+}(typeof self !== 'undefined' ? self : this, function(tools) {
+
+	const MaterialListItem = function(params) {
+		this.icon = params.icon;
+		this.text = params.text;
+		this.value = params.value;
+		this.enable = tools.isComponentEnabled(params);
+
+		this.click = typeof params.click !== 'function' ? params.click : function(vm, event) {
+			//the timeout means the parent list's selection handler will execute
+			//even if the clicked item gets disabled as a result of click processing
+			setTimeout(() => params.click(vm, event));
+		};
+	};
+	MaterialListItem.prototype = {
+		'getCss': function() {
+			return {
+				'mdc-deprecated-list-item--disabled': !this.enable()
+			};
+		},
+		'getAttrs': function() {
+			return {
+				'data-value': this.value
+			};
+		}
+	};
+
+	return {
+		'viewModel': MaterialListItem,
+		'template':
+`<li class="mdc-deprecated-list-item" role="option" data-bind="click: click, css: getCss(), attr: getAttrs()">
+	<span class="mdc-deprecated-list-item__ripple"></span>
+	<!-- ko if: icon -->
+	<span class="mdc-deprecated-list-item__graphic material-icons">{{icon}}</span>
+	<!-- /ko -->
+	<!-- ko if: text -->
+	<span class="mdc-deprecated-list-item__text">{{text}}</span>
+	<!-- /ko -->
+	<!-- ko ifnot: text -->
+	<span class="mdc-deprecated-list-item__text" data-bind="template: {'nodes': $componentTemplateNodes, 'data': $parent}"></span>
+	<!-- /ko -->
+</li>`
+	};
+}));
---
+++ material-tooltip/material-tooltip.js
@@ -0,0 +1,56 @@
+﻿(function(root, factory) {
+	const toolsPackageName = '@skolaris/knockout-tools',
+		mdcToolsPackageName = '@knockout-mdc/mdc-tools',
+		mdcTooltipPackageName = '@material/tooltip';
+
+	if (typeof define === 'function' && define.amd) {
+		//AMD. Register as an anonymous module.
+		define([toolsPackageName, mdcToolsPackageName, mdcTooltipPackageName], factory);
+	}
+	else if (typeof module === 'object' && module.exports) {
+		//Node. Does not work with strict CommonJS, but only CommonJS-like environments that support module.exports like Node.
+		module.exports = factory(require(toolsPackageName), require(mdcToolsPackageName), require(mdcTooltipPackageName));
+	}
+	else {
+		//Browser globals (root is window)
+		root.knockoutMdc = root.knockoutMdc || {};
+		root.knockoutMdc['material-tooltip'] = factory(root.knockoutTools, root.knockoutMdc.mdcTools, root.mdc.tooltip);
+	}
+}(typeof self !== 'undefined' ? self : this, function(tools, mdcTools, materialTooltip) {
+
+	const MaterialTooltip = function() {
+		this.id = tools.getGuid();
+
+		//component lifetime
+		this.mdcTooltip = null;
+	};
+	MaterialTooltip.prototype = {
+		'koDescendantsComplete': function(node) {
+			if (!node.isConnected)
+				return;
+
+			const previousSibling = node.previousElementSibling;
+			if (previousSibling)
+				previousSibling.setAttribute('aria-describedby', this.id);
+
+			//use the node directly as the tooltip root
+			node.id = this.id;
+			node.classList.add('mdc-tooltip');
+			node.setAttribute('role', 'tooltip');
+			node.setAttribute('aria-hidden', 'true');
+			this.mdcTooltip = new materialTooltip.MDCTooltip(node);
+			mdcTools.setMdcComponent(node, this.mdcTooltip);
+		},
+		'dispose': function() {
+			this.mdcTooltip?.destroy();
+		}
+	};
+
+	return {
+		'viewModel': MaterialTooltip,
+		'template':
+`<div class="mdc-tooltip__surface mdc-tooltip__surface-animation">
+	<div class="mdc-tooltip__label" data-bind="template: {'nodes': $componentTemplateNodes, 'data': $parent}"></div>
+</div>`
+	};
+}));
